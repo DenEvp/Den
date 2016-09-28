e91a9bc55b5ff37f93f59a2d7511e680192aabbc
DenEvp <den_evp@mail.ru>
2016-09-28 22:09:33 +0300

    Project from IntelliJ

---
+++ src/Den/UITest.java
@@ -0,0 +1,11 @@
+package Den;
+
+/**
+ * Created by Den on 28.09.2016.
+ */
+public class UITest {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("test2");
+    }
+}
